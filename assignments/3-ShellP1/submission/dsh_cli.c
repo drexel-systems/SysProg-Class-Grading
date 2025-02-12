aa3371337af2fb3977ef7f5825e49f94755467cd
mehrojalimov <alimovmekhroj01@gmail.com>
2025-02-12 00:10:58 -0500

    finishing the assignment

--- assignments/3-ShellP1/submission/dsh_cli.c
+++ assignments/3-ShellP1/submission/dsh_cli.c
@@ -4,6 +4,7 @@
 
 #include "dshlib.h"
 
+
 /*
  * Implement your main function by building a loop that prompts the
  * user for input.  Use the SH_PROMPT constant from dshlib.h and then
@@ -44,12 +45,49 @@
  *
  *  See the provided test cases for output expectations.
  */
-int main()
-{
-    char *cmd_buff;
+
+// the handler for the input parsing
+int input_parse(char *cmd_buff) {
+    if (fgets(cmd_buff, ARG_MAX, stdin) == NULL) {
+        printf("\n");
+        return -1;
+    }
+    cmd_buff[strcspn(cmd_buff, "\n")] = '\0';
+    return (strcmp(cmd_buff, EXIT_CMD) == 0) ? -1 : 0;
+}
+
+int main() {
+    char cmd_buff[SH_CMD_MAX];
     int rc = 0;
     command_list_t clist;
 
-    printf(M_NOT_IMPL);
-    exit(EXIT_NOT_IMPL);
+    while (1) {
+        printf("%s", SH_PROMPT);
+        
+        if (input_parse(cmd_buff) == -1) {
+            break;
+        }
+
+        // Parse and process command
+        rc = build_cmd_list(cmd_buff, &clist);
+        
+        if (rc == OK) {
+            printf(CMD_OK_HEADER, clist.num);
+            for (int i = 0; i < clist.num; i++) {
+                if (strlen(clist.commands[i].args) > 0) {
+                    printf("<%d> %s [%s]\n", i + 1, 
+                           clist.commands[i].exe, 
+                           clist.commands[i].args);
+                } else {
+                    printf("<%d> %s\n", i + 1, clist.commands[i].exe);
+                }
+            }
+        } else if (rc == WARN_NO_CMDS) {
+            printf(CMD_WARN_NO_CMD);
+        } else if (rc == ERR_TOO_MANY_COMMANDS) {
+            printf(CMD_ERR_PIPE_LIMIT, CMD_MAX);
+        }
+    }
+    
+    return 0;
 }
--- assignments/3-ShellP1/submission/dshlib.c
+++ assignments/3-ShellP1/submission/dshlib.c
@@ -4,7 +4,7 @@
 #include <ctype.h>
 
 #include "dshlib.h"
-
+int split_commands(char *cmd_line, command_list_t *clist);
 /*
  *  build_cmd_list
  *    cmd_line:     the command line from the user
@@ -32,8 +32,80 @@
  *  Standard Library Functions You Might Want To Consider Using
  *      memset(), strcmp(), strcpy(), strtok(), strlen(), strchr()
  */
-int build_cmd_list(char *cmd_line, command_list_t *clist)
-{
-    printf(M_NOT_IMPL);
-    return EXIT_NOT_IMPL;
+
+ //this is our split command handler
+int split_commands(char *cmd_line, command_list_t *clist) {
+    char *pipe_saveptr;
+    char *command_segment = strtok_r(cmd_line, "|", &pipe_saveptr);
+
+    while (command_segment) {
+        // Check command limit
+        if (clist->num >= CMD_MAX) {
+            return ERR_TOO_MANY_COMMANDS;
+        }
+
+        while (isspace(*command_segment)) command_segment++;
+
+        // Store command segment
+        strcpy(clist->commands[clist->num].exe, command_segment);
+        clist->num++;
+
+        command_segment = strtok_r(NULL, "|", &pipe_saveptr);
+    }
+
+    return OK;
+}
+
+
+int build_cmd_list(char *cmd_line, command_list_t *clist) {
+    if (!cmd_line || !clist) {
+        return ERR_CMD_OR_ARGS_TOO_BIG;
+    }
+
+    memset(clist, 0, sizeof(command_list_t));
+
+    int split_result = split_commands(cmd_line, clist);
+    if (split_result != OK) {
+        return split_result;
+    }
+
+    // Parse each command for arguments
+    for (int i = 0; i < clist->num; i++) {
+        char *arg_saveptr;
+        char *token = strtok_r(clist->commands[i].exe, " \t\n", &arg_saveptr);
+
+        if (!token) {
+            continue;
+        }
+
+        if (strlen(token) >= EXE_MAX) {
+            return ERR_CMD_OR_ARGS_TOO_BIG;
+        }
+
+        strcpy(clist->commands[i].exe, token);
+
+        char args[ARG_MAX] = "";
+        token = strtok_r(NULL, " \t\n", &arg_saveptr);
+
+        while (token) {
+            size_t curr_len = strlen(args);
+            size_t token_len = strlen(token);
+
+            if (curr_len + token_len + 2 >= ARG_MAX) {
+                return ERR_CMD_OR_ARGS_TOO_BIG;
+            }
+
+            if (curr_len > 0) {
+                strcat(args, " ");
+            }
+            strcat(args, token);
+            token = strtok_r(NULL, " \t\n", &arg_saveptr);
+        }
+
+        if (strlen(args) > 0) {
+            strcpy(clist->commands[i].args, args);
+        }
+    }
+
+    return (clist->num > 0) ? OK : WARN_NO_CMDS;
 }
